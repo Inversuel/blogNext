e5bef3d45e440a8cb0b48d04700aef6ae4c12c97
Karol Maj <invers1996@gmail.com>
2024-02-21 23:16:08 +0100

    Add @next/third-parties dependency and integrate Google Analytics and Google Tag Manager

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,18 @@ import Nav from "@/component/nav/nav";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Inter } from "next/font/google";
+import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={`${inter.variable} font-sans min-h-screen overflow-x-hidden`}>
+    <main
+      className={`${inter.variable} font-sans min-h-screen overflow-x-hidden`}
+    >
+      <GoogleAnalytics gaId="G-SWFXX4XCLL" />
+      <GoogleTagManager gtmId="GT-WB7XNP9X" />
+
       <Nav />
       <Component {...pageProps} />
     </main>
